5dead9ad22ea3ed1e6eb0209a1df7cfde30fd3b2
godofwar-mmd <wolf.id.clashroyale@gmail.com>
2025-05-09 09:27:59 +0330

    Create translations.js

---
+++ translations.js
@@ -0,0 +1,10 @@
+const translations = {
+    fa: { name: "فارسی", flag: "https://flagcdn.com/w20/ir.png", home: "خانه", products: "محصولات", contact: "ارتباط با ما", telegram: "تلگرام", youtube: "یوتیوب", selectGame: "انتخاب بازی", game1: "خدای جنگ ۲", game2: "رزیدنت اویل ۴" },
+    en: { name: "English", flag: "https://flagcdn.com/w20/gb.png", home: "Home", products: "Products", contact: "Contact Us", telegram: "Telegram", youtube: "YouTube", selectGame: "Select Game", game1: "God of War 2", game2: "Resident Evil 4" },
+    fr: { name: "Français", flag: "https://flagcdn.com/w20/fr.png", home: "Accueil", products: "Produits", contact: "Contactez-nous", telegram: "Telegram", youtube: "YouTube", selectGame: "Choisir un jeu", game1: "Dieu de la guerre 2", game2: "Resident Evil 4" },
+    de: { name: "Deutsch", flag: "https://flagcdn.com/w20/de.png", home: "Startseite", products: "Produkte", contact: "Kontakt", telegram: "Telegram", youtube: "YouTube", selectGame: "Spiel auswählen", game1: "God of War 2", game2: "Resident Evil 4" },
+    es: { name: "Español", flag: "https://flagcdn.com/w20/es.png", home: "Inicio", products: "Productos", contact: "Contáctanos", telegram: "Telegram", youtube: "YouTube", selectGame: "Seleccionar juego", game1: "God of War 2", game2: "Resident Evil 4" },
+    ru: { name: "Русский", flag: "https://flagcdn.com/w20/ru.png", home: "Главная", products: "Продукты", contact: "Связаться с нами", telegram: "Телеграм", youtube: "Ютуб", selectGame: "Выбор игры", game1: "God of War 2", game2: "Resident Evil 4" },
+    cn: { name: "中文", flag: "https://flagcdn.com/w20/cn.png", home: "首页", products: "产品", contact: "联系我们", telegram: "电报", youtube: "油管", selectGame: "选择游戏", game1: "战神2", game2: "生化危机4" },
+    ar: { name: "العربية", flag: "https://flagcdn.com/w20/sa.png", home: "الصفحة الرئيسية", products: "المنتجات", contact: "اتصل بنا", telegram: "تلغرام", youtube: "يوتيوب", selectGame: "اختر لعبة", game1: "اله الحرب ٢", game2: "ريزدنت ايفل ٤" }
+};
